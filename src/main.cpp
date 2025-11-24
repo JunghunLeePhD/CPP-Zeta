c89f85292c31d56b3fd883c4eea2d136dd8609b7
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-24 11:10:59 +0000

    Add/Update (`Theta.h/cpp`): `Theta` class in `Zeta` module

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include "Theta.h"
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    int size = 10;
+    double start_value = 0.0;
+    double step = 0.1f;
+
+    std::vector<double> v(size);
+    std::generate(v.begin(), v.end(), [current = start_value, &step]() mutable{
+        double value = current;
+        current += step;
+
+        double theta = Zeta::Theta::value(value);
+        std::cout << theta << std::endl;
+
+        return value;
+    });
 
     return 0;
 }
